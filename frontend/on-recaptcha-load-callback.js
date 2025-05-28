3dabadfb1c3c1f8ed67529d6b1393d69577832a1
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2025-05-28 21:53:37 +0200

    Fixed recaptcha check

---
+++ frontend/on-recaptcha-load-callback.js
@@ -0,0 +1,10 @@
+window.onRecaptchaLoadCallback= function () {
+  if (window.ENV && window.ENV.RECAPTCHA_SITE_KEY) {
+    grecaptcha.render('recaptcha-container', {
+      sitekey: window.ENV.RECAPTCHA_SITE_KEY
+    });
+  } else {
+    console.error("reCAPTCHA site key missing in env.js");
+    document.getElementById('message').textContent = "reCAPTCHA configuration error.";
+  }
+};
